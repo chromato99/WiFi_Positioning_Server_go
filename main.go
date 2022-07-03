006db58db9252d40858532b6d5f7ffddd7b35005
chromato99 <chromato99@gmail.com>
2022-07-03 11:16:25 +0900

    add multithread set

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"runtime"
+
 	"github.com/chromato99/WiFi_Positioning_Server_go/core"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	runtime.GOMAXPROCS(4)
 	router := gin.Default()
 	router.POST("/test", core.Test)
 	router.POST("/add", core.AddData)
